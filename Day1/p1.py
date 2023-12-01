f712201da2e43e9ace97faccc1482ddae4628dff
Arosh Heenkenda <ahee0011@student.monash.edu>
2023-12-01 18:01:11 +1100

    Complete Day 1 Part 1

---
+++ Day1/p1.py
@@ -0,0 +1,26 @@
+
+def p1():
+    f = open("input1.txt")
+
+    lines = [line.strip() for line in f.readlines()]
+
+    total = 0
+
+    for line in lines:
+        
+        num = ""
+
+        for let in line:
+            if let.isnumeric():
+                num += let
+        
+        total += int(num[0]+num[-1])
+        
+
+    f.close()
+    return total
+
+if __name__ == "__main__":
+
+    out = p1()
+    print(out)
