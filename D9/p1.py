bf54138577bee0f693d0781fb9b0886138b6ab21
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2023-12-15 16:13:10 +1100

    Complete Day 9 Part 1

---
+++ D9/p1.py
@@ -0,0 +1,43 @@
+
+
+def p1():
+
+    f = open("in.txt")
+    history = [[int(num) for num in line.strip().split()] for line in f.readlines()]
+
+    ex_sum = 0
+
+    for his in history:
+
+        cur = his
+        extra = []
+        extra.append(cur)
+
+        while True:
+
+            if len(set(cur)) == 1:
+                if cur[0] == 0:
+                    break
+
+            new_list = []
+
+            for i in range(1, len(cur)):
+                new_list.append(cur[i] - cur[i-1])
+            
+            extra.append(new_list)
+            cur = new_list
+        
+        data = 0
+
+        for i in range(len(extra) - 2, -1, -1):
+            data += extra[i][-1]
+        
+        ex_sum += data
+    
+    return ex_sum
+
+
+if __name__ == "__main__":
+
+    out = p1()
+    print(out)
